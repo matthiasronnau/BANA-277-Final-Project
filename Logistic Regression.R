ad3827927b25cddb7c8d48b0daebc95510127762
matthasronnau@berkeley.edu <matthiasronnau@berkeley.edu>
2021-03-18 17:20:09 -0700

    Balance data

--- Logistic Regression.R
+++ Logistic Regression.R
@@ -10,12 +10,14 @@ setwd("~/The University of California, Irvine/Winter Quarter/Customer and Social
 
 #Set options
 options(scipen = 999)
+set.seed(277)
 
 ###Load Packages
 library(readr)
 library(lubridate)
 library(chron)
 library(dplyr)
+library(ROSE)
 library(igraph)
 
 #Read the Data into R
@@ -27,13 +29,20 @@ out_degree_dataframe <- read_csv("Data/Cleaned Data/out_degree_dataframe.csv")
 
 #Get data from the sample that is part of the subcomponent
 data <- subset(samp, samp$product_id %in% ids$id)
-rm(samp)
-rm(ids)
+rm(samp, ids)
 data_no_missing <- na.omit(data)
 data_no_missing$purchase <- ifelse(data_no_missing$event_type == "purchase", 1, 0)
 data_no_missing$event_time <- as_datetime(data_no_missing$event_time)
 data_no_missing$time <- chron(times = format(data_no_missing$event_time, format = "%H:%M:%S"))
 rm(data)
+nrow(subset(data_no_missing, data_no_missing$purchase == 1))
+nrow(data_no_missing)
+
+#Oversample the data for unbiased estimates for logistic regression
+over <- sample_frac(ovun.sample(purchase ~ ., data = data_no_missing, method = "over")$data, 0.5)
+nrow(subset(over, over$purchase == 1))
+nrow(over)
+rm(data_no_missing)
 
 #Get Network Statistics
 g_subcomponent <- graph_from_data_frame(g)
@@ -78,36 +87,32 @@ head(network_stats)
 rm(close_df, between_df, hub_df, authority_df)
 
 #Join both datasets together to be used for the logistic regression
-merged <- inner_join(g, data_no_missing, by = c("Source" = "product_id"))
+merged <- inner_join(g, over, by = c("Source" = "product_id"))
 merged$view <- ifelse(merged$event_type == "view", 1, 0)
 merged$cart <- ifelse(merged$event_type == "cart", 1, 0)
 merged$purchase <- ifelse(merged$event_type == "purchase", 1, 0)
-#merged <- left_join(data_no_missing, g, by = c("product_id" = "Source"))
+rm(g)
 
-sources <- unique(merged$Source)
-target_source <- subset(merged, merged$Target %in% sources)
+target_source <- subset(merged, merged$Target %in% unique(merged$Source))
 neighbor_stats <- summarize(group_by(target_source, Target), nghb_mn_price = mean(price), 
                             nghb_avg_pct_views = mean(view),
                             nghb_avg_pct_cart = mean(cart),
                             nghb_avg_pct_purchase = mean(purchase))
+rm(target_source)
 names(neighbor_stats)[names(neighbor_stats) == "Target"] <- "product_id"
 head(neighbor_stats)
 
-logistic_data <- left_join(left_join(left_join(left_join(neighbor_stats, data_no_missing, by = "product_id"), in_degree_dataframe, by = c("product_id" = "Source")), out_degree_dataframe,
+logistic_data <- left_join(left_join(left_join(left_join(neighbor_stats, over, by = "product_id"), in_degree_dataframe, by = c("product_id" = "Source")), out_degree_dataframe,
                     by = c("product_id" = "Source")), network_stats, by = c("product_id" = "id"))
+rm(over, neighbor_stats, in_degree_dataframe, out_degree_dataframe, network_stats)
 
 logistic_model <- glm(purchase ~ log(price) + time + in_degree + out_degree + closeness + betweenness + hub_score + authority_score +
                         nghb_mn_price + nghb_avg_pct_views + nghb_avg_pct_cart + nghb_avg_pct_purchase, family = "binomial", data = logistic_data)
 summary(logistic_model)
 
-updated_logistic_model <- glm(purchase ~ log(price) + time + closeness + betweenness,
-                              family = "binomial", data = logistic_data)
+updated_logistic_model <- glm(purchase ~ log(price) + time + in_degree + closeness + betweenness + 
+                                hub_score + nghb_avg_pct_views, family = "binomial", data = logistic_data)
 summary(updated_logistic_model)
-
-
-
-
-
 
 
 
